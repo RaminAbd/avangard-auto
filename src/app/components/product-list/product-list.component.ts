e258805bb40ac3b9bbe52f0f9e892a2fdb84ed1c
Ramin Abdulaev <rmnabdulaev@gmail.com>
2023-05-30 23:20:57 +0400

    fixing bug in product list(0 count adding to card)

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,12 +1,23 @@
-import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input, EventEmitter, Output, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent {
-  @Input() Products:any[];
-  @Output() getProduct:any = new EventEmitter();
-  @Output() addToCart:any = new EventEmitter();
+export class ProductListComponent implements AfterViewInit{
+  @ViewChild('scrollContainer', { static: true }) scrollContainer: ElementRef;
+  viewInitialized: boolean = false;
+  Products: any[] = []
+  @Input() set products(e: any) {
+    this.Products = e;
+    if(this.viewInitialized){
+      this.scrollContainer.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+  @Output() getProduct: any = new EventEmitter();
+  @Output() addToCart: any = new EventEmitter();
+  ngAfterViewInit(): void {
+    this.viewInitialized = true;
+  }
 }
